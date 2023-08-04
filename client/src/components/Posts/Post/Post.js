2fc82805a4fcfe1a0326a4b1e8a13618d408553d
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-08-04 22:25:01 +0530

    ...

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,14 +6,15 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import { useDispatch } from 'react-redux';
 import moment from 'moment';
-
-import { likePost, deletePost } from '../../../actions/posts';
+import { useHistory } from 'react-router-dom';
+import { getPost, likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const user = JSON.parse(localStorage.getItem('profile'));
+  const history = useHistory();
 
   const Likes = () => {
     if (post.likes.length > 0) {
@@ -28,30 +29,40 @@ const Post = ({ post, setCurrentId }) => {
     return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
   };
 
+  const openPost = (e) => {
+    // dispatch(getPost(post._id, history));
+
+    history.push(`/posts/${post._id}`);
+  };
+
   return (
     <Card className={classes.card} raised elevation={6}>
-      <ButtonBase>
-        
-      </ButtonBase>
-      <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
-      <div className={classes.overlay}>
-        <Typography variant="h6">{post.name}</Typography>
-        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-      </div>
-      {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-        <div className={classes.overlay2}>
-          <Button onClick={() => setCurrentId(post._id)} style={{ color: 'white' }} size="small">
-            <MoreHorizIcon fontSize="default" />
-          </Button>
+      <ButtonBase
+        className={classes.cardAction}
+        onClick={openPost}
+      >
+
+
+        <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
+        <div className={classes.overlay}>
+          <Typography variant="h6">{post.name}</Typography>
+          <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
-      )}
-      <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-      </div>
-      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
-      </CardContent>
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+          <div className={classes.overlay2}>
+            <Button onClick={() => setCurrentId(post._id)} style={{ color: 'white' }} size="small">
+              <MoreHorizIcon fontSize="default" />
+            </Button>
+          </div>
+        )}
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+        </CardContent>
+      </ButtonBase>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
           <Likes />
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, getPostsBySearch, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPostsBySearch, createPost, updatePost, likePost, deletePost, getPost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
@@ -12,5 +12,6 @@ router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
+router.get('/:id', getPost);
 
 export default router;
